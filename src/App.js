a15876246a783a5143cffc7ac4b1a7aae297d444
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2021-07-25 15:45:22 -0400

    Revert "Adjusted default animation timing"
    This reverts commit 3bcc420352e6bfad0acb1c60d6bbc7a38aaa6e33.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import { useState } from "react";
-
-import Greeting from "./components/Greeting";
+import Greeting from './components/Greeting'
 
 function App() {
-  const [isGreeting, setIsGreeting] = useState(true);
-
-  const handleOnFinished = () => {
-    setIsGreeting(false);
-  };
-
-  return (isGreeting ? <Greeting title="Hi, I'm Ethan!" subtitle="Welcome to my website." onFinished={handleOnFinished} /> :
+  return (
     <div className="App">
-      <h1>Hello, world!</h1>
+      <Greeting title="Hi, I'm Ethan!" subtitle="Welcome to my website." />
     </div>
   );
 }
--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,36 +1,44 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import './Greeting.css';
 
-function Greeting({ title, subtitle, duration = 4000, subtitleDelay = 1500, onFinished }) {
-    useEffect(() => {
-        const titleElement = document.querySelector(".greetingTitle");
-        const subtitleElement = document.querySelector(".greetingSubtitle");
+function Greeting({ title, subtitle, duration = 5000, subtitleDelay = 2000 }) {
+    const [isVisible, setIsVisible] = useState(true);
 
-        // Fade in title
-        titleElement.classList.add("fadeIn");
-
-        // Fade in subtitle after subtitleDelay (default: 1500 ms)
-        setTimeout(() => {
-            subtitleElement.classList.add("fadeIn");
-        }, subtitleDelay);
-
-        // Fade out title and subtitle after specified duration (default: 4000 ms)
-        setTimeout(() => {
-            titleElement.classList.replace("fadeIn", "fadeOut");
-            subtitleElement.classList.replace("fadeIn", "fadeOut");
-        }, duration);
-
-        // call onFinished function after finished (fade out takes 1000 ms)
-        setTimeout(onFinished, duration + 1000);
-    });
-
-    return (
+    const greetingElement = (
         <header className="greetingContainer" >
             <h1 className="greetingTitle" >{title}</h1>
             <p className="greetingSubtitle" >{subtitle}</p>
         </header>
     );
+
+    useEffect(() => {
+        if (isVisible) {
+            const titleElement = document.querySelector(".greetingTitle");
+            const subtitleElement = document.querySelector(".greetingSubtitle");
+
+            // Fade in title
+            titleElement.classList.add("fadeIn");
+
+            // Fade in subtitle after subtitleDelay (default: 2000ms)
+            setTimeout(() => {
+                subtitleElement.classList.add("fadeIn");
+            }, subtitleDelay);
+
+            // Fade out title and subtitle after specified duration (default: 5000ms)
+            setTimeout(() => {
+                titleElement.classList.replace("fadeIn", "fadeOut");
+                subtitleElement.classList.replace("fadeIn", "fadeOut");
+            }, duration);
+
+            // Hide component after finished (fade out takes 1000ms)
+            setTimeout(() => {
+                setIsVisible(false);
+            }, duration + 1000);
+        };
+    });
+
+    return isVisible ? greetingElement : null;
 };
 
 export default Greeting;
