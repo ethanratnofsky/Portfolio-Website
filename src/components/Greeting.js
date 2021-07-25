e1b08db9f6a4216f34eaada4c7a1287547ffee6d
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2021-07-25 14:30:03 -0400

    Refactored Greeting React component, added optional duration and subtitleDelay props

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -2,13 +2,13 @@ import { useEffect, useState } from 'react';
 
 import './Greeting.css';
 
-function Greeting(props) {
+function Greeting({ title, subtitle, duration = 5000, subtitleDelay = 2000 }) {
     const [isVisible, setIsVisible] = useState(true);
 
     const greetingElement = (
         <header className="greetingContainer" >
-            <h1 className="greetingTitle" >{props.title}</h1>
-            <p className="greetingSubtitle" >{props.subtitle}</p>
+            <h1 className="greetingTitle" >{title}</h1>
+            <p className="greetingSubtitle" >{subtitle}</p>
         </header>
     );
 
@@ -17,22 +17,24 @@ function Greeting(props) {
             const titleElement = document.querySelector(".greetingTitle");
             const subtitleElement = document.querySelector(".greetingSubtitle");
 
+            // Fade in title
             titleElement.classList.add("fadeIn");
 
+            // Fade in subtitle after subtitleDelay (default: 2000ms)
             setTimeout(() => {
                 subtitleElement.classList.add("fadeIn");
-            }, 2000);
+            }, subtitleDelay);
 
+            // Fade out title and subtitle after specified duration (default: 5000ms)
             setTimeout(() => {
-                titleElement.classList.remove("fadeIn");
-                subtitleElement.classList.remove("fadeIn");
-                titleElement.classList.add("fadeOut");
-                subtitleElement.classList.add("fadeOut");
-            }, 5000);
+                titleElement.classList.replace("fadeIn", "fadeOut");
+                subtitleElement.classList.replace("fadeIn", "fadeOut");
+            }, duration);
 
+            // Hide component after finished (fade out takes 1000ms)
             setTimeout(() => {
                 setIsVisible(false);
-            }, 6000);
+            }, duration + 1000);
         };
     });
 
