a9b53f096618ceef2442b7f6ce4f109e30cde6a7
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2021-07-31 21:32:53 -0400

    Refactor using useRef

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,31 +1,31 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 import './Greeting.css';
 
 function Greeting({ title, subtitle, duration = 4000, subtitleDelay = 1500, onFinished }) {
-    useEffect(() => {
-        const titleElement = document.querySelector(".greeting-title");
-        const subtitleElement = document.querySelector(".greeting-subtitle");
+    const titleElement = useRef(null);
+    const subtitleElement = useRef(null);
 
+    useEffect(() => {
         // Fade in subtitle after subtitleDelay (default: 1500 ms)
         setTimeout(() => {
-            subtitleElement.classList.add("fade-in");
+            subtitleElement.current.classList.add("fade-in");
         }, subtitleDelay);
 
         // Fade out title and subtitle after specified duration (default: 4000 ms)
         setTimeout(() => {
-            titleElement.classList.replace("fade-in", "fade-out");
-            subtitleElement.classList.replace("fade-in", "fade-out");
+            titleElement.current.classList.replace("fade-in", "fade-out");
+            subtitleElement.current.classList.replace("fade-in", "fade-out");
         }, duration);
 
         // call onFinished function after finished (fade out takes 1000 ms)
         setTimeout(onFinished, duration + 1000);
-    }, []);
+    }, [duration, onFinished, subtitleDelay]);
 
     return (
         <header className="greeting-container" >
-            <h1 className="greeting-title fade-in" >{title}</h1>
-            <p className="greeting-subtitle" >{subtitle}</p>
+            <h1 className="greeting-title fade-in" ref={titleElement} >{title}</h1>
+            <p className="greeting-subtitle" ref={subtitleElement} >{subtitle}</p>
         </header>
     );
 };
