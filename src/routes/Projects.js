130a5bd955baf2e404e6988a93e88a441b124544
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2021-08-13 17:41:41 -0400

    Implement project tiles

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,8 +1,19 @@
+import ProjectTile from "../components/ProjectTile"
+
+import "../styles/Projects.css"
+
 function Projects() {
     return (
-        <>
-            <p>Projects</p>
-        </>
+        <div className="projects" >
+            <h1 className="page-title" >Projects</h1>
+            <div className="tiles-container" >
+                <ProjectTile title="Title" description="Description" deliverables="" year="2021" link="https://google.com" />
+                <ProjectTile title="Title" description="Description" deliverables="" year="2021" link="https://google.com" />
+                <ProjectTile title="Title" description="Description" deliverables="" year="2021" link="https://google.com" />
+                <ProjectTile title="Title" description="Description" deliverables="" year="2021" link="https://google.com" />
+                <ProjectTile title="Title" description="Description" deliverables="" year="2021" link="https://google.com" />
+            </div>
+        </div>
     )
 };
 
