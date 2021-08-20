b3c50bc10c1c7f603a4c233e9bd12eb02c19b2a8
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2021-08-20 13:45:10 -0400

    Edit background info

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -9,11 +9,11 @@ function Home() {
     return (
         <div className="home-container">
             <CartoonEthan />
-            <div className="info-container" >
+            <div className="background-info-container" >
                 <h1>Hi, I'm Ethan Ratnofsky!</h1>
                 <p>
                     Let me tell you a little bit about myself!
-                    I was born and raised in the Greater Boston area, and I still refer to Newton, MA as "home, sweet home."
+                    I was born and raised in the Greater Boston area, and Newton, MA is still "home, sweet home."
                     However, I am currently an undergraduate student studying Computer Science at Vanderbilt University in Nashville, TN.
                     I am a passionate developer, curious entrepreneur, and creative artist.
                     Explore my website for the proof in the pudding!
