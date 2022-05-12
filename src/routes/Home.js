f946a79df72cbd96f5292fb1ab555724fde08cdd
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2022-05-12 00:54:54 -0500

    Update info blurb, info container background, and icon titles

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -11,21 +11,31 @@ function Home() {
     return (
         <div className="home-container">
             <CartoonEthan />
-            <div className="background-info-container" >
-                <h1>Hi, I'm Ethan Ratnofsky!</h1>
-                <p>
-                    Let me tell you a little bit about myself!
-                    I was born and raised in the Greater Boston area, and Newton, MA is still "home, sweet home."
-                    However, I am currently an undergraduate student studying Computer Science at Vanderbilt University in Nashville, TN.
-                    I am a passionate developer, curious entrepreneur, and creative artist.
-                    Explore my website for the proof in the pudding!
-                </p>
+            <div className="info-container" >
+                <div className="background-info" >
+                    <h1>Hello, World!</h1>
+                    <p>
+                        My name is Ethan Ratnofsky, and I am an undergraduate student studying Computer Science at Vanderbilt University in Nashville, TN.<br />
+                        <br />
+                        I am a passionate developer, curious entrepreneur, and creative artist born and raised in Newton, MA.
+                        I love adventuring and traveling, and, although I haven't checked off many destinations just yet, I hope to travel the world.
+                        Currently, I plan to pursue a career in software development.<br />
+                        <br />
+                        Welcome to my portfolio website - feel free to explore!
+                    </p>
+                </div>
                 <div className="social-links" >
-                    <a className="icon-link" href="https://github.com/ethanratnofsky" target="_blank" rel="noreferrer" ><GitHubIcon title="Ethan Ratnofsky | GitHub" /></a>
-                    <a className="icon-link" href="https://www.linkedin.com/in/ethan-ratnofsky/" target="_blank" rel="noreferrer" ><LinkedInIcon title="Ethan Ratnofsky | LinkedIn" /></a>
+                    <a className="icon-link" href="https://github.com/ethanratnofsky" target="_blank" rel="noreferrer" >
+                        <GitHubIcon title="GitHub | Ethan Ratnofsky" />
+                    </a>
+                    <a className="icon-link" href="https://www.linkedin.com/in/ethan-ratnofsky/" target="_blank" rel="noreferrer" >
+                        <LinkedInIcon title="LinkedIn | Ethan Ratnofsky" />
+                    </a>
                     {/* TODO: Secure mailing system? */}
-                    <Link className="icon-link" to="/coming-soon" ><GmailIcon title="Ethan Ratnofsky | Mail" /></Link>
-                    {/* <a className="icon-link" href="/coming-soon" ><GmailIcon title="Ethan Ratnofsky | Mail" /></a> */}
+                    <Link className="icon-link" to="/coming-soon" >
+                        <GmailIcon title="Contact Me | Ethan Ratnofsky" />
+                    </Link>
+                    {/* <a className="icon-link" href="/coming-soon" ><GmailIcon title="Contact Me | Ethan Ratnofsky" /></a> */}
                 </div>
             </div>
         </div>
